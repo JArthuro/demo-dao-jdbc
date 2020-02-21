57face2bfecb2be5ecd97f1b597e583547ef5106
Jose Arthuro <zearthuro@hotmail.com>
2020-02-21 16:47:53 -0300

    Delete Implemetation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -92,6 +92,8 @@ PreparedStatement st = null;
 			st.setDouble(4, obj.getBaseSalary());
 			st.setInt(5, obj.getDepartment().getId());
 			st.setInt(6, obj.getId());
+			
+			st.executeUpdate();
 		}
 		
 		catch (SQLException e) {
@@ -107,8 +109,23 @@ PreparedStatement st = null;
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-
+		
+		PreparedStatement st = null;
+		try {
+			
+			st = conn.prepareStatement(
+			"DELETE FROM seller WHERE Id = ?");
+			st.setInt(1, id);			
+			st.executeUpdate();
+		}
+		
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
